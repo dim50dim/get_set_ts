cbe5b688346a5a5cdb8f23545f8b638bbcf2c0ee
¨dim50dim¨ <zheliezkov@gmail.com>
2025-04-17 21:59:37 +0200

    error with _n1 .

--- sprint_12.ts
+++ sprint_12.ts
@@ -2,10 +2,13 @@
 class T01{
     private _n1 : number = 33;
     private _n2 : number = 55;
+    public get _n1 () : number {
+        return this._n1;
+    }
 }
 
 const obj1 = new T01();
-console.log(obj1._n1);
+console.log(obj1.n1);
 
 
 
